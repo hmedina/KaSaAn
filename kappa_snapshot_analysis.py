24cfc865d6c8aa44caf3f4c77b312491817e2113
Hector <hector.f.medina.a@gmail.com>
2017-02-03 00:51:22 -0500

    Overhaul, completely new paradigm and organization.

--- kappa_snapshot_analysis.py
+++ kappa_snapshot_analysis.py
@@ -1,58 +1,25 @@
 #! python.exe
 import re
-import os
 
 
-def complex_sizes(kappa_file):
-    # First read the file and get, for each line (i.e. one complex per line), the length & abundance
-    with open(kappa_file, 'r') as kf:
-        raw_complex_size = []
-        raw_complex_num = []
-        for line in kf:
-            if (line[0] != '#') & (line != '\n'):
-                raw_complex_size.append(line.count('('))
-                raw_complex_num.append(int(re.search('%init:\s(\d+)\s', line).group(1)))
-    # Make sure we have both length & abundance of each complex
-    assert len(raw_complex_size) == len(raw_complex_num)
+class KappaComplex:
+    """Class for representing & manipulating Kappa complexes."""
 
-    # Expand the list by repeating the size of the complex, the number of times it appears in the mix
-    # (i.e. if a dimer appears three times, the list will contain at least the number 2 in triplicate)
-    c_sizes = []
-    for i, x in enumerate(raw_complex_num):
-        c_sizes += [raw_complex_size[i] for _ in range(x)]
+    def __init__(self, kappa_expression):
+        self.kappa_expression = kappa_expression
 
-    c_sizes.sort()
-    return c_sizes
+    def get_number_of_agent(self, agent_expression):
+        """Prints the number of times a given agent appears in the complex. It supports specifying parts of the
+        signature for the agent, specifically state information. I.e. looking for `A(s~x)` is supported."""
+        matches = re.match('([a-zA-Z]+\w*)\(([\w!,~]*)\)', agent_expression)
+        agent_name = matches.group(1)
+        agent_signature = matches.group(2)
+        pattern = agent_name + '\([\w!,~]*' + agent_signature + '[\w!,~]*\)'
+        matches = re.findall(pattern, self.kappa_expression)
+        print(len(matches))
 
-
-def get_sorted_snapshots(folder, prefix):
-    # Problem: snap1, snap10, snap2, snap3 ... are in order lexically, but not by event. Ergo, I must parse the
-    #  directory. I need the list of event numbers at at which the snapshot were taken. I can then reconstruct the
-    # filename from that number and return a sorted list.
-    snapshot_indexes = []
-    for f in os.listdir(folder):
-        m = re.search('(?<=' + prefix + ')(\d+).ka', f)
-        if m:
-            snapshot_indexes.append(int(m.group(1)))
-
-    # Sort the indexes, rendered numerically above, to get the snapshots in the right order
-    snapshot_indexes.sort()
-
-    # Now reconstruct the name of the snapshot, including the folder
-    sorted_snapshots = []
-    for i in snapshot_indexes:
-        sorted_snapshots.append(folder + '/' + prefix + str(i) + '.ka')
-
-    return sorted_snapshots, snapshot_indexes
-
-
-def time_series_distribution(sorted_snapshots):
-    # Parse the snapshots, to get their data.
-    snap_distributions = []
-    for snap in sorted_snapshots:
-        snap_distributions.append(complex_sizes(snap))
-
-    # Make sure we have an entry for every snapshot
-    assert len(sorted_snapshots) == len(snap_distributions)
-
-    return snap_distributions
+    def get_constituent_agents(self):
+        """Prints the set of agents that make up the complex."""
+        matches = re.findall('([a-zA-Z]+\w*)\([\w!,~]*\)', self.kappa_expression)
+        agents = set(matches)
+        print(agents)
