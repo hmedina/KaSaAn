6946b652993b45b6b4d6c467891bf6c4e645fcd6
Hector <hector.f.medina.a@gmail.com>
2017-02-13 20:19:27 -0500

    Rolling back the get_number_of_given_agent method to a simpler version. Pattern matching against kappa expressions proved too difficult (I couldn't figure out how to deal with out-of-order sites for example), so now method only accepts the agent's name (i.e. no signature support).

--- kappa_snapshot_analysis.py
+++ kappa_snapshot_analysis.py
@@ -6,9 +6,6 @@ class KappaComplex:
     """Class for representing  Kappa complexes. I.e. 'A(b!1),B(a!1,c!2),C(b!2,a!3),A(c!3,s~x)'. Notice these must be
     connected components. E.g. each line of a kappa snapshot is a KappaComplex."""
 
-    ka_nom_expr = '[a-zA-Z]+\w*'
-    ka_sig_expr = '[\w!,~\s]*'
-
     def __init__(self, kappa_expression):
         self.kappa_expression = kappa_expression
 
@@ -17,14 +14,12 @@ class KappaComplex:
         signature for the agent, specifically state information. I.e. looking for 'A(s~x)' is supported."""
 
         # Get agent name & signature, and make sure input is a valid kappa expression
-        matches = re.match('(' + self.ka_nom_expr + ')\((' + self.ka_sig_expr + ')\)', kappa_query)
-        assert matches, 'Query is not a valid kappa expression >' + kappa_query + '< forgot parenthesis?'
+        matches = re.match('([a-zA-Z]+\w*)\(\)', kappa_query)
+        assert matches, 'Invalid query >' + kappa_query + '<, only agent name + parentheses "A()" accepted.'
         query_name = matches.group(1)
-        query_signature = matches.group(2)
 
         # Pad known signature with pattern to match against user unmentioned sites stated in snapshot
-        pattern = query_name + '\(' + self.ka_sig_expr + query_signature + self.ka_sig_expr + '\)'
-        matches = re.findall(pattern, self.kappa_expression)
+        matches = re.findall(query_name + '\([^)]*\)', self.kappa_expression)
         return len(matches)
 
     def get_number_of_bonds(self):
@@ -35,15 +30,14 @@ class KappaComplex:
     def get_size_of_complex(self):
         """Returns the size, in agents, of this complex. This works by counting the number of agent signatures with
         their open/close parentheses."""
-        matches = re.findall('\(' + self.ka_sig_expr + '\)', self.kappa_expression)
+        matches = re.findall('\([^)]*\)', self.kappa_expression)
         return int(len(matches))
 
     def get_constituent_agents(self):
         """Returns the set of agents that make up the complex."""
-        matches = re.findall('(' + self.ka_nom_expr + ')\(' + self.ka_sig_expr + '\)', self.kappa_expression)
+        matches = re.findall('([a-zA-Z]+\w*)\([^)]*\)', self.kappa_expression)
         agents = set(matches)
         return agents
 
 
-class KappaSnapshot:
-    raise NotImplemented
+# TODO class KappaSnapshot:
