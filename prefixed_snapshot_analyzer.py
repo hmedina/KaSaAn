000d4e32e9911363d01bc1a45f699e17d461f9db
hmedina <hector.f.medina.a@gmail.com>
2017-06-03 00:10:40 -0400

    Extended documentation of prefixed_snapshot_analyzer.

--- prefixed_snapshot_analyzer.py
+++ prefixed_snapshot_analyzer.py
@@ -6,13 +6,14 @@ import argparse
 
 
 def snapshot_analyzer(base_directory, snap_prefix, verbosity):
+    # Get the file names of snapshots in specified directory that fit the pattern [prefix]snap[number].ka
     snap_names = glob.glob(base_directory + snap_prefix + 'snap*.ka')
     snap_num = len(snap_names)
     if verbosity:
         print("Found " + str(snap_num) + " snapshots in directory " + base_directory + ' with prefix <' + snap_prefix + '> .')
+    assert snap_num >= 2, "Problem: less than 2 snapshots were found."
 
-    assert snap_num >= 2, "Error: less than 2 snapshots were found."
-
+    # For each snapshot, get the size distribution & update the results dictionary {size: abundance}
     cum_dist = {}
     for snap_name in snap_names:
         dist = KappaSnapshot(snap_name).get_size_distribution()
@@ -22,24 +23,26 @@ def snapshot_analyzer(base_directory, snap_prefix, verbosity):
             else:
                 cum_dist[key] = dist[key]
 
-    with open(base_directory + snap_prefix + 'distribution_cumulative.csv', 'w') as csv_file:
+    # Save the cumulative distribution to file
+    cum_dist_file_name = base_directory + snap_prefix + 'distribution_cumulative.csv'
+    with open(cum_dist_file_name, 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['Size', 'Cumulative Abundance'])
         for key, value in cum_dist.items():
             writer.writerow([key, value])
-
     if verbosity:
-        print("Cumulative distribution written to file.")
+        print('Cumulative distribution written to file: ' + cum_dist_file_name)
 
-
-    with open(base_directory + snap_prefix + 'distribution_mean.csv', 'w') as csv_file:
+    # Save the mean distribution to file; it's identical to the cumulative but divided by the total number of snapshots
+    # found in the directory
+    mean_dist_file_name = base_directory + snap_prefix + 'distribution_mean.csv'
+    with open(mean_dist_file_name, 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['Size', 'Mean Abundance'])
         for key, value in cum_dist.items():
             writer.writerow([key, value/snap_num])
-
     if verbosity:
-        print("Mean distribution written to file.")
+        print('Mean distribution written to file: ' + mean_dist_file_name)
 
 
 if __name__ == '__main__':
