3d7e34bf3e46f738a2328e9e4d8e364c23d9bfe5
hmedina <hector.f.medina.a@gmail.com>
2018-03-09 14:59:41 -0500

    Added a working legend on a secondary axis.

--- trace_movie_maker.py
+++ trace_movie_maker.py
@@ -4,70 +4,101 @@ import glob
 import re
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from snapshot_visualizer import process_snapshot, snapshot_composition_simple, colorize_agents
+from matplotlib.collections import PatchCollection
+from snapshot_visualizer import process_snapshot, snapshot_composition_simple, colorize_agents, snapshot_legend_simple
 from kappa4_snapshot_analysis import KappaSnapshot
 
 
 # ToDo make this a function!
+
 # Helper function to sort file names
-numbers = re.compile(r'(\d+)')
 def numerical_sort(value):
-    parts = numbers.split(value)
+    parts = re.compile(r'(\d+)').split(value)
     parts[1::2] = map(int, parts[1::2])
+
     return parts
 
 
 # Get & sort the file names
-snapshot_directory = './models/trace_vis/'
-snap_names = sorted(glob.glob(snapshot_directory + 'snapshot.*.ka'), key=numerical_sort)
-print('Found ' + str(len(snap_names)) + ' snapshots in ' + snapshot_directory)
-snapshot_list = []
-for file_name in snap_names:
-    snapshot_list.append(KappaSnapshot(file_name))
+def find_snapshot_files(base_dir='./'):
+    snap_names = sorted(glob.glob(base_dir + 'snapshot.*.ka'), key=numerical_sort)
+    snapshot_list = []
+    for file_name in snap_names:
+        snapshot_list.append(KappaSnapshot(file_name))
+
+    return snapshot_list
 
 
 # Define consistent coloring scheme & maximum mass
-max_mass = 0
-agent_set = set()
-for snap in snapshot_list:
-    agent_set.update(snap.get_agent_types_present())
-    if max_mass < snap.get_total_mass():
-        max_mass = snap.get_total_mass()
+def define_agent_list_and_max_mass(snap_list):
+    max_mass = 0
+    agent_set = set()
+    for snap in snap_list:
+        agent_set.update(snap.get_agent_types_present())
+        if max_mass < snap.get_total_mass():
+            max_mass = snap.get_total_mass()
 
-my_color_scheme = colorize_agents(list(agent_set))
+    return list(agent_set), max_mass
+
+
 
 
 # Figure size & target resolution of mass
+my_snapshot_directory = './models/trace_vis/'
+my_snapshots = find_snapshot_files(my_snapshot_directory)
+print('Found ' + str(len(my_snapshots)) + ' snapshots in ' + my_snapshot_directory)
+
+my_agent_list, my_max_mass = define_agent_list_and_max_mass(my_snapshots)
+print('Found ' + str(len(my_agent_list)) + ' agents.')
+
+my_color_scheme = colorize_agents(my_agent_list)
+
 my_vis_mode = 'mass'
-my_x_res = 1200
-my_y_res = 800
+data_XY_ratio = 1
+my_x_res = 1000
+my_y_res = my_x_res * data_XY_ratio
 
-fig, ax = plt.subplots()
+fig_width = 16 #width of figure, in inches
+my_fig = plt.figure(figsize=[fig_width, fig_width/2])
+data_ax = my_fig.add_subplot(121, aspect=1)
+lgnd_ax = my_fig.add_subplot(122, aspect=1)
 
+# Define the animation, i.e. the left axis
 artist_list = []
-for snap in snapshot_list:
+for my_snap in my_snapshots:
     ars = []
-    snap_scale = snap.get_total_mass() / max_mass
-    my_data = process_snapshot(snap)
+    snap_scale = my_snap.get_total_mass() / my_max_mass
+    my_data = process_snapshot(my_snap)
     rectangles, maxi = snapshot_composition_simple(data=my_data, color_scheme=my_color_scheme, vis_mode=my_vis_mode,
                                                    x_res=my_x_res * snap_scale, y_res=my_y_res * snap_scale)
     for r in rectangles:
-        ar = ax.add_patch(r)
+        ar = data_ax.add_patch(r)
         ars.append(ar)
-    ars.append(ax.text(0, my_y_res, 'Time ' + str(snap.get_snapshot_time()) + ' ; final time ' + str(snapshot_list[-1].get_snapshot_time()),
-                       horizontalalignment='left', verticalalignment='bottom'))
-    ars.append(ax.text(my_x_res, my_y_res, 'Mass present ' + str(snap.get_total_mass()),
-                       horizontalalignment='right', verticalalignment='bottom'))
+    ars.append(data_ax.text(0, my_y_res,
+                            'Time ' + str(my_snap.get_snapshot_time()) + ' ; final time ' + str(my_snapshots[-1].get_snapshot_time()),
+                            horizontalalignment='left', verticalalignment='bottom'))
+    ars.append(data_ax.text(my_x_res, my_y_res, 'Mass present ' + str(my_snap.get_total_mass()),
+                            horizontalalignment='right', verticalalignment='bottom'))
     artist_list.append(ars)
 
 
-ax.set_xlim(left=0, right=my_x_res)
-ax.set_ylim(bottom=0, top=my_y_res)
-ax.axis('off')
+data_ax.set_xlim(left=0, right=my_x_res)
+data_ax.set_ylim(bottom=0, top=my_y_res)
+data_ax.axis('off')
 
+# Define the legend, i.e. the right axis
+columns = 2
+legend_squares, legend_texts = snapshot_legend_simple(color_scheme=my_color_scheme, col_num=columns)
+lgnd_ax.add_collection(PatchCollection(legend_squares, match_original=True))
+for text in legend_texts:
+    lgnd_ax.add_artist(text)
+
+lgnd_ax.set_xlim(left=0, right=columns)
+lgnd_ax.set_ylim(top=1, bottom=-divmod(len(my_color_scheme), columns)[0] - 1)
+lgnd_ax.axis('off')
 
 # Make movie out of list
-ani = animation.ArtistAnimation(fig=fig, artists=artist_list, interval=500, repeat_delay=1000)
+ani = animation.ArtistAnimation(fig=my_fig, artists=artist_list, interval=500, repeat_delay=1000)
 
 #ani.save('prozone.mp4')
 plt.show()
