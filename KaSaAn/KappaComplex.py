1580869999cfa54fbaa4e5a9ac237cfc0d65f784
hmedina <hector.f.medina.a@gmail.com>
2018-12-07 19:53:47 -0500

    Added type support to KappaComplex class.

--- KaSaAn/KappaComplex.py
+++ KaSaAn/KappaComplex.py
@@ -1,42 +1,52 @@
 #! /usr/bin/python3
 
 import re
+from typing import List, Set, Dict
 from .KappaAgent import KappaAgent
 
 
 class KappaComplex:
-    """Class for representing Kappa complexes. I.e. 'A(b[1] s{u}), B(a[1] c[2]), C(b[2] a[3]), A(c[3] s{x})'. Notice
-     these must be connected components."""
+    """Class for representing Kappa complexes. I.e. 'A(b[1] s{u}[.]), B(a[1] c[2]), C(b[2] a[3]), A(c[3] s[.]{x})'.
+    Notice these must be connected components."""
+    kappa_expression: str
 
-    def __init__(self, kappa_expression):
-        """The constructor also removes spaces from the expression."""
-        self.kappa_expression = kappa_expression
+    def __init__(self, expression: str):
+        self.kappa_expression = expression.strip()
 
-    def get_number_of_bonds(self):
+    def __repr__(self):
+        return 'KappaComplex("{0}")'.format(self.kappa_expression)
+
+    def __str__(self):
+        return self.kappa_expression
+
+    def get_number_of_bonds(self) -> int:
         """Returns the number of bonds in the complex."""
         matches = re.findall('\[\d+\]', self.kappa_expression)
         return int(len(matches) / 2)
 
-    def get_size_of_complex(self):
+    def get_size_of_complex(self) -> int:
         """Returns the size, in agents, of this complex. This works by counting the number of agent signatures with
         their open/close parentheses."""
         matches = re.findall('\([^)]*\)', self.kappa_expression)
         return int(len(matches))
 
-    def get_agent_types(self):
-        """Returns the set of agents that make up the complex. This works by counting the number of non-digit-leading
-        words that have a set of open/close parenthesis directly after them."""
+    def get_agent_types(self) -> Set[str]:
+        """Returns the set of agent names that make up the complex. This works by counting the number of
+        non-digit-leading words that have a set of open/close parenthesis directly after them."""
         matches = re.findall('([a-zA-Z][\w\-_]*)\([^)]*\)', self.kappa_expression)
         agents = set(matches)
         return agents
 
-    def get_agents(self):
+    def get_agents(self) -> List[KappaAgent]:
         """Returns a list of KappaAgents, filled with agents plus their signatures, present in this complex."""
-        agent_list = self.kappa_expression.split(', ')
-        agent_list = [KappaAgent(item) for item in agent_list]
+        # replace commas with spaces, then split string into a list at closing parenthesis
+        agent_exp_list = self.kappa_expression.replace(',', ' ').split(')')
+        # strip of padding whitespace, attach chomped closing parenthesis, ignore empty last entry
+        agent_exp_list = [item.strip() + ')' for item in agent_exp_list[0:-1]]
+        agent_list = [KappaAgent(item) for item in agent_exp_list]
         return agent_list
 
-    def get_number_of_embeddings_of_agent(self, query):
+    def get_number_of_embeddings_of_agent(self, query: str) -> int:
         """Returns the number of embeddings the query agent has on the KappaComplex. Does not follow bonds,
         so effectively the query must be a single-agent."""
         kappa_query = KappaComplex(query)
@@ -59,7 +69,7 @@ class KappaComplex:
                         match_number += 1
         return match_number
 
-    def get_complex_composition(self):
+    def get_complex_composition(self) -> Dict[str, int]:
         """Returns a dictionary where the key is an agent name, and the value the number of times that agent appears in
         this complex."""
         composition = {}
@@ -67,7 +77,7 @@ class KappaComplex:
             composition[agent] = self.get_number_of_embeddings_of_agent(agent + '()')
         return composition
 
-    def get_number_of_embeddings_of_complex(self, query):
+    def get_number_of_embeddings_of_complex(self, query: str) -> int:
         """Returns the number of embedding the query complex has on the KappaComplex. Follows bonds. WIP"""
         raise NotImplemented
 
