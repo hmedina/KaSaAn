1418af6c38296edd073294facc788c65a7469052
Hector Medina <hector.f.medina.a@gmail.com>
2019-02-28 17:01:29 -0500

    KaSaAn.functions now declares get_potential_of_snapshot function, so it can be imported as from KaSaAn.functions import get_potential_of_snapshot

--- KaSaAn/functions/__init__.py
+++ KaSaAn/functions/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from .prefixed_snapshot_analyzer import prefixed_snapshot_analyzer
+from .catalytic_potential import get_potential_of_snapshot, get_potential_of_folder
 from .numerical_sort import numerical_sort
 from .plot_filtered_distributions import plot_filtered_dist
-from .catalytic_potential import get_potential_of_folder
+from .prefixed_snapshot_analyzer import prefixed_snapshot_analyzer
 from .snapshot_visualizer import render_snapshot
 from .trace_movie_maker import movie_from_snapshots
