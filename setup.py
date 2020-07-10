14884f23ddc7dd45a1f37830a2c1559b171e7604
hmedina <hector.f.medina.a@gmail.com>
2020-07-10 18:21:55 -0400

    Fix #3
    Subpackages now explicitly called for setup tools.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='KaSaAn',
     version='0.1.dev0',
-    packages=['KaSaAn'],
+    packages=['KaSaAn', 'KaSaAn.core', 'KaSaAn.functions'],
     package_dir={'': '.'},
     url='https://github.com/hmedina/KaSaAn',
     license='MIT',
