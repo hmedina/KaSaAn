985d5fd4d5aa03193712c0c5745268136aeeef2f
Hector Medina <hector.f.medina.a@gmail.com>
2020-07-31 05:25:11 -0400

    Update __init__.py
    Declare new function in the subpackage so it can be imported elsewhere; reorder functions for alphabetical consistency.

--- KaSaAn/functions/__init__.py
+++ KaSaAn/functions/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from .catalytic_potential import get_potential_of_snapshot, get_potential_of_folder
+from .find_snapshot_names import find_snapshot_names
+from .numerical_sort import numerical_sort
 from .observable_plotter import observable_file_reader, observable_list_axis_annotator
 from .observable_coplotter import observable_coplot_axis_annotator, observable_multi_data_axis_annotator
-from .numerical_sort import numerical_sort
 from .plot_filtered_distributions import plot_filtered_dist
 from .prefixed_snapshot_analyzer import prefixed_snapshot_analyzer
 from .snapshot_visualizer_patchwork import render_snapshot_as_patchwork
