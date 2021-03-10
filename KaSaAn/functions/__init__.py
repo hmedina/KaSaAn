796cbaa3458cdf0cd26a537d8c1b7d954b50ce54
Hector Medina <hector.f.medina.a@gmail.com>
2021-03-10 18:01:52 -0500

    Scripts now are named like their entry-point destination console commands. Removed unused utilities.

--- KaSaAn/functions/__init__.py
+++ KaSaAn/functions/__init__.py
@@ -3,13 +3,11 @@
 """Utility functions used stand-alone, or as the main component of entry-points (defined under `KaSaAn.scripts`
  sub-module)."""
 
-from .catalytic_potential import _get_potential_of_snapshot, get_potential_of_folder
+from .catalytic_potential import get_potential_of_folder
 from .find_snapshot_names import find_snapshot_names
 from .numerical_sort import numerical_sort
 from .observable_plotter import observable_file_reader, observable_list_axis_annotator
 from .observable_coplotter import observable_coplot_axis_annotator, _observable_multi_data_axis_annotator
-from .plot_filtered_distributions import plot_filtered_dist
-from .prefixed_snapshot_analyzer import prefixed_snapshot_analyzer
 from .snapshot_visualizer_patchwork import render_snapshot_as_patchwork
 from .snapshot_visualizer_network import render_snapshot_as_plain_graph
 from .snapshot_visualizer_subcomponent import render_complexes_as_plain_graph
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 setup(
     entry_points={
         'console_scripts': [
-            'kappa_catalytic_potential = KaSaAn.scripts.catalytic_potential:main',
-            'kappa_observable_plotter = KaSaAn.scripts.observable_plotter:main',
-            'kappa_observable_coplotter = KaSaAn.scripts.observable_coplotter:main',
-            'kappa_snapshot_largest_complex_time = KaSaAn.scripts.graph_largest_complex_composition:main',
-            'kappa_snapshot_visualizer_patchwork = KaSaAn.scripts.snapshot_visualizer_patchwork:main',
-            'kappa_snapshot_visualizer_network = KaSaAn.scripts.snapshot_visualizer_network:main',
-            'kappa_snapshot_visualizer_subcomponent = KaSaAn.scripts.snapshot_visualizer_subcomponent:main',
-            'kappa_trace_movie_maker = KaSaAn.scripts.trace_movie_maker:main',
+            'kappa_catalytic_potential = KaSaAn.scripts.kappa_catalytic_potential:main',
+            'kappa_observable_plotter = KaSaAn.scripts.kappa_observable_plotter:main',
+            'kappa_observable_coplotter = KaSaAn.scripts.kappa_observable_coplotter:main',
+            'kappa_snapshot_largest_complex_time = KaSaAn.scripts.kappa_snapshot_largest_complex_time:main',
+            'kappa_snapshot_visualizer_patchwork = KaSaAn.scripts.kappa_snapshot_visualizer_patchwork:main',
+            'kappa_snapshot_visualizer_network = KaSaAn.scripts.kappa_snapshot_visualizer_network:main',
+            'kappa_snapshot_visualizer_subcomponent = KaSaAn.scripts.kappa_snapshot_visualizer_subcomponent:main',
+            'kappa_trace_movie_maker = KaSaAn.scripts.kappa_trace_movie_maker:main',
         ]
     }
 )
