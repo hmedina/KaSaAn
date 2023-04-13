1c5373f545465b4f848c3e7a3028712f193eaafe
Hector Medina <hector.f.medina.a@gmail.com>
2023-04-13 17:54:34 -0400

    Support Paths and strings in find_snapshot_names

--- KaSaAn/functions/find_snapshot_names.py
+++ KaSaAn/functions/find_snapshot_names.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python3
 
-import pathlib
-import warnings
-from typing import List
+from pathlib import Path
+from typing import List, Union
 from .numerical_sort import numerical_sort
+import warnings
 
 
-def find_snapshot_names(target_directory: str = '.', name_pattern: str = 'snap*.ka') -> List[str]:
+def find_snapshot_names(target_directory: Union[str, Path] = '.', name_pattern: str = 'snap*.ka') -> List[str]:
     """Given a target directory (default `./`), and a snapshot naming scheme (default `snap*.ka`), return a list of
      snapshot names sorted ascending by a numerical specifier. By default, KaSim inserts the event number into a
      snapshot's name."""
-    target_path = pathlib.Path(target_directory)
+    if type(target_directory) == str:
+        target_path = Path(target_directory)
+    elif type(target_directory) == Path:
+        pass
+    else:
+        raise ValueError(
+            'Expected a string or a Pathlib.Path object for parameter "target_directory", got {}'.format(
+                type(target_directory)))
     if not target_path.exists():
-        raise ValueError('Directory <' + target_directory + '> not found.')
+        raise ValueError('Directory {} not found'.format(target_directory))
     snap_names = [str(file_path) for file_path in target_path.glob(name_pattern)]
     if len(snap_names) < 2:
         warnings.warn('Found <{}> snapshots in directory <{}> using file naming pattern <{}>.'.format(
