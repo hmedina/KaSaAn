5b4a35ee31078987870d14484966fc636d7b89ce
Hector Medina <hector.f.medina.a@gmail.com>
2023-09-16 01:29:07 -0400

    Update contact map docs, example & image

--- models/contact_map/contact_map_demo.py
+++ models/contact_map/contact_map_demo.py
@@ -6,10 +6,10 @@ from KaSaAn.core import KappaContactMap
 my_contact_map = KappaContactMap('inputs.ka')
 fig, ax = plt.subplots(figsize=(6, 6))
 
-my_contact_map.move_agent_to('Fitz', 5, 5)
-my_contact_map.move_agent_to('Foo', 10, 15)
-my_contact_map.move_agent_to('Bar', 12.5, 2.5)
-my_contact_map.move_agent_to('Baz', 0, 12.5)
+my_contact_map.move_agent_to('Fitz', 10, 10)
+my_contact_map.move_agent_to('Foo', 20, 30)
+my_contact_map.move_agent_to('Bar', 25, 5)
+my_contact_map.move_agent_to('Baz', 0, 25)
 
 my_contact_map.rotate_all_sites_of('Foo', 190)
 my_contact_map.rotate_all_sites_of('Baz', -180)
