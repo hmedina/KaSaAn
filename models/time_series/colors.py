2c05ba9532ae04b9285cdc65fcae3f32e22ddb58
Hector Medina <hector.f.medina.a@gmail.com>
2024-03-27 02:16:35 -0400

    Test file: tab20c indexing too

--- models/time_series/colors.py
+++ models/time_series/colors.py
@@ -1,1 +1,6 @@
-{'A()': 'tab:blue', 'B(n[1]), C(m[1])': 'tab:green'}
+#{'A()': 'tab:blue', 'B(n[1]), C(m[1])': 'tab:green'}
+{
+    'A()': 'plt.cm.tab20c(1)',
+    'B()': 'plt.cm.tab20c(2)',
+    'C()': 'plt.cm.tab20c(3)'
+}
