ef7c190bb32c8ffb4ba9867cab9485e876ce40aa
Hector Medina <hector.f.medina.a@gmail.com>
2025-02-07 22:31:29 -0500

    NetMap & embed_and_map: documented!
    * Added NetMap class and embed_and_map function to the module's export list, allowing their details to be documented by pdoc3

--- KaSaAn/core/__init__.py
+++ KaSaAn/core/__init__.py
@@ -3,14 +3,15 @@
 """This is the core API. These sub-modules contain the classes used to analyze Kappa expressions."""
 
 from .KappaSnapshot import KappaSnapshot
-from .KappaComplex import KappaComplex
+from .KappaComplex import KappaComplex, NetMap, embed_and_map
 from .KappaBond import KappaBond
 from .KappaAgent import KappaAgent, KappaToken
 from .KappaSite import KappaPort, KappaCounter
 from .KappaContactMap import KappaContactMap
 from .KappaRule import KappaRule
 
-__all__ = ['KappaSnapshot', 'KappaComplex',
+__all__ = ['KappaSnapshot',
+           'KappaComplex', 'NetMap', 'embed_and_map',
            'KappaBond', 'KappaAgent', 'KappaToken',
            'KappaCounter', 'KappaPort',
            'KappaContactMap']
